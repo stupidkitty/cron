dca581ce5a911871440246b7c9f62da8965cd306
stupidkitty <adx.informer@protonmail.com>
2020-03-16 22:53:12 +0300

    Create events name collection

---
+++ src/Event/Events.php
@@ -0,0 +1,9 @@
+<?php
+namespace SK\CronModule\Event;
+
+class Events
+{
+    const TASK_PASSED = 'cron.task_passed';
+    const TASK_FAILED = 'cron.task_passed';
+    const TASK_FINISHED = 'cron.task_passed';
+}
