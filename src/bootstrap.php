db7e3b2d2b1e4adb0a40b034d92d8f5aeb27be00
stupidkitty <adx.informer@protonmail.com>
2020-03-16 22:51:55 +0300

    Add bootstrao with configure container

---
+++ src/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+namespace SK\CronModule;
+
+use Yii;
+
+$container = Yii::$container;
+
+$container->setSingleton(Executor\ScheduledExecutorInterface::class, Executor\ScheduledExecutor::class);
+$container->setSingleton(Scheduler\SchedulerInterface::class, Scheduler\TaskScheduler::class);
+$container->setSingleton(Handler\HandlerFactoryInterface::class, Handler\HandlerFactory::class);
